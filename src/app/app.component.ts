a381e7cd27915e0a542464c5fbe998b6295773ee
Ezequias Aramburu <ezequias@MacBook-Pro-de-Ezequias.local>
2020-08-20 17:48:48 -0300

    add star rating view & logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rating-star';
+
+  stars: number[] = [1, 2, 3, 4, 5];
+  selectedValue: number = 0;
+
+  constructor() { }
+  
+  ngOnInit() {
+  }
+  
+  count(star) {
+    if (star === this.selectedValue)
+      this.selectedValue = this.selectedValue - 1;
+    else
+      this.selectedValue = star;
+    console.log('Value of star', star);
+  }
 }
